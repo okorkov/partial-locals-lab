8d5b01ece455e9d9f75e97182507b17ce87e4421
okorkov <alex.okorkov@gmail.com>
2021-01-20 14:45:05 -0800

    commit tree 79c1bbf7ec2b73263ad7f85940e2e965ff6329b2

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -18,6 +18,8 @@ class StudentsController < ApplicationController
 
   def show
     @student = Student.find(params[:id])
+  
+    
   end
 
   def index
--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,8 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(input)
+    Student.where("name LIKE ?", "%#{input}%")
+  end
 end
